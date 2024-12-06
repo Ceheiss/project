b1b64c4e9adcb8a39b3a1b3b0dfae75a99474269
crisheiss <cristobal.heiss@joingsg.com>
2024-12-06 16:30:18 +0100

    add note details page

--- frontend/app/notes/[id]/page.tsx
+++ frontend/app/notes/[id]/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 export default function NoteDetails({ params }: { params: Promise<{ id: string }> }) {
   const [id, setId] = useState<string | null>(null);
+  const [note, setNote] = useState<any>(null);
 
   useEffect(() => {
     async function fetchParams() {
@@ -13,14 +14,32 @@ export default function NoteDetails({ params }: { params: Promise<{ id: string }
     fetchParams();
   }, [params]);
 
-  if (!id) {
+  useEffect(() => {
+    fetch(`http://localhost:5000/notes/${id}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setNote(data[0]);
+      });
+  }, [id]);
+
+  if (!note) {
     return <div>Loading...</div>;
   }
 
   return (
     <div>
       <h1>Note Details</h1>
-      <p>Note ID: {id}</p>
+      <h3>Discipline:</h3>
+      <p>{note.discipline}</p>
+      <h3>Techniques:</h3>
+      <p>{note.techniques}</p>      
+      <h3>Feel Scale:</h3>
+      <p>{note.feel_rating}</p>
+      <h3>Insights:</h3>
+      <p>{note.insights}</p>
     </div>
   );
 }
